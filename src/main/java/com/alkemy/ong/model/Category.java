c4fc2ab730a5a7df6ec84dc6771da6b4165ded59
Alejandro Aguirre <aguirreee21@gmail.com>
2022-05-22 22:31:47 -0300

    Create Category Model

---
+++ src/main/java/com/alkemy/ong/model/Category.java
@@ -0,0 +1,47 @@
+package com.alkemy.ong.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE categories SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
+@Table(name = "categories")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Name cannot be empty")
+    private String name;
+
+    @NotNull(message = "Description cannot be empty")
+    private String description;
+
+    @NotNull(message = "Image cannot be empty")
+    private String image;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createDateTime;
+
+    @UpdateTimestamp
+    private LocalDateTime updateDateTime;
+
+    private boolean deleted = Boolean.FALSE;
+
+}
