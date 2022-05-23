833f1217beee8a19cf25b3f9ae1c402d60036746
Martinpozzoli <martinpozzoli2@gmail.com>
2022-05-23 20:22:24 -0300

    News entity and repository added

--- src/main/java/com/alkemy/ong/repository/INewsRepository.java
+++ src/main/java/com/alkemy/ong/repository/INewsRepository.java
@@ -1,6 +1,5 @@
 package com.alkemy.ong.repository;
 
-import com.alkemy.ong.model.News;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface INewsRepository extends JpaRepository<News, Long> {
