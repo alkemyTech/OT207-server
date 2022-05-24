24fb50b1064a7af87cff66e9b817acf4f56ae9ff
Alexis Molina <84826653+softwareaim@users.noreply.github.com>
2022-05-24 17:41:17 -0300

    Revert "Feature/ot207 15 organizations"

--- src/main/java/com/alkemy/ong/repository/OrganizationsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.ong.repository;
-import com.alkemy.ong.model.Organizations;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrganizationsRepository extends JpaRepository<Organizations, Long> {
-
-}
