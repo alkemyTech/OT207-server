2aa64cf9890e3f42fb0abc1ae86a7407158277ef
Alexis Molina <alexis_molina789@hotmail.com>
2022-06-03 19:34:18 -0300

    slides controller added

---
+++ src/main/java/com/alkemy/ong/mapper/SlideMapper.java
@@ -0,0 +1,32 @@
+package com.alkemy.ong.mapper;
+
+import com.alkemy.ong.dto.SlidesRequestDTO;
+import com.alkemy.ong.dto.SlidesResponseDTO;
+import com.alkemy.ong.model.Slides;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+
+@Component
+public class SlideMapper {
+
+    public Slides requestDto2SlidesEntity(@NotNull SlidesRequestDTO requestDTO){
+        Slides entity = new Slides();
+        entity.setImageUrl(requestDTO.getImageUrl());
+        entity.setOrderSlides(requestDTO.getOrderSlides());
+        entity.setOrganizations(requestDTO.getOrganizations());
+        entity.setText(requestDTO.getText());
+        return entity;
+    }
+
+    public SlidesResponseDTO entitySlides2responseDto(@NotNull Slides entity){
+        SlidesResponseDTO responseDTO = new SlidesResponseDTO();
+        responseDTO.setId(entity.getId());
+        responseDTO.setImageUrl(entity.getImageUrl());
+        responseDTO.setOrderSlides(entity.getOrderSlides());
+        responseDTO.setText(entity.getText());
+        return responseDTO;
+    }
+
+
+}
