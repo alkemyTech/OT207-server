cd072aca726f3f2615db792ae02ed20a4628f12c
Alan Shalom <alan.shalom.96@gmail.com>
2022-06-03 14:55:07 -0300

    feature/OT207-37

--- src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
+++ src/main/java/com/alkemy/ong/service/impl/OrganizationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.service.impl;
 
 import com.alkemy.ong.dto.OrganizationDTO;
+import com.alkemy.ong.dto.OrganizationUpdateDTO;
 import com.alkemy.ong.exception.NotFoundException;
 import com.alkemy.ong.mapper.OrganizationMapper;
 import com.alkemy.ong.model.Organization;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrganizationServiceImpl implements IOrganizationService {
@@ -32,4 +34,21 @@ public class OrganizationServiceImpl implements IOrganizationService {
         List<OrganizationDTO> dtoList = organizationMapper.organizationEntityList2DTOList(entities);
         return dtoList;
     }
+
+    @Override
+    public Organization updateOrganizationDto(OrganizationUpdateDTO orgUpdate) throws NotFoundException {
+        Optional<Organization> org = organizationRepository.findById(orgUpdate.getId());
+
+        if (org.isEmpty()) {
+            throw new NotFoundException("Organization id does not exist" + orgUpdate.getId());
+        } else {
+            return organizationMapper.OrganizationUpdateDTO2Entity(orgUpdate, org.get());
+        }
+    }
+
+    @Override
+    public void updateOrganization(Organization org) {
+        organizationRepository.save(org);
+       }
+
 }
