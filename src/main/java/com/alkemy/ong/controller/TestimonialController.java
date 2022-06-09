07c9e2b121dd9d879d09839798cdceb4e8f66a01
Alejandro Aguirre <aguirreee21@gmail.com>
2022-06-09 19:29:00 -0300

    Ultimate changes

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -1,10 +1,12 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.TestimonialDto;
+import com.alkemy.ong.exception.BadRequestException;
 import com.alkemy.ong.service.ITestimonialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,10 @@ public class TestimonialController {
     private ITestimonialService testimonialService;
 
     @PostMapping
-    public ResponseEntity<TestimonialDto> createTestimonial(@Valid @RequestBody TestimonialDto dto) {
+    public ResponseEntity<TestimonialDto> createTestimonial(@Valid @RequestBody TestimonialDto dto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()){
+            throw new BadRequestException(bindingResult);
+        }
         return new ResponseEntity<>(testimonialService.save(dto), HttpStatus.CREATED);
     }
 
