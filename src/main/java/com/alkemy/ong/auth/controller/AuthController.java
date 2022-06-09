f08fd3b7aca043c24f3be3d9384480613cf78fa2
Alan Shalom <alan.shalom.96@gmail.com>
2022-06-09 18:33:24 -0300

    update develop

--- src/main/java/com/alkemy/ong/auth/controller/AuthController.java
+++ src/main/java/com/alkemy/ong/auth/controller/AuthController.java
@@ -4,23 +4,15 @@ import com.alkemy.ong.auth.dto.*;
 import com.alkemy.ong.auth.service.JwtUtils;
 import com.alkemy.ong.auth.service.impl.UserDetailsCustomService;
 import com.alkemy.ong.exception.BadRequestException;
-import com.alkemy.ong.exception.ForbiddenException;
-import com.amazonaws.services.sns.model.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import javax.validation.constraints.Email;
-import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
