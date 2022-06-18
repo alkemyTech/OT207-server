84327911b6dfb1c431a7a9b1fb59089e659adcfb
Diego A. Sejas <dsejas2015@gmail.com>
2022-06-18 01:28:18 -0300

    Endpoint Actualización Comentarios

--- src/main/java/com/alkemy/ong/controller/CommentaryController.java
+++ src/main/java/com/alkemy/ong/controller/CommentaryController.java
@@ -1,15 +1,20 @@
 package com.alkemy.ong.controller;
 
-import com.alkemy.ong.auth.service.impl.UserDetailsCustomService;
 import com.alkemy.ong.dto.CommentaryBodyDTO;
+import com.alkemy.ong.dto.CommentaryDTO;
+import com.alkemy.ong.exception.BadRequestException;
 import com.alkemy.ong.service.ICommentaryService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/comments")
@@ -17,11 +22,19 @@ public class CommentaryController {
     
     @Autowired
     private ICommentaryService commentaryService;
-    
+
     @GetMapping
     public ResponseEntity<List<CommentaryBodyDTO>> getComments () {
         List<CommentaryBodyDTO> listDto = commentaryService.getCommentaries();
         return ResponseEntity.ok().body(listDto);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<CommentaryBodyDTO> updateCommentary(@Valid @RequestBody CommentaryBodyDTO dto, BindingResult bindingResult,
+                                                           @PathVariable Long id , HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            throw new BadRequestException(bindingResult);
+        }
+        return new ResponseEntity<>(commentaryService.update(id, dto ,request), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
