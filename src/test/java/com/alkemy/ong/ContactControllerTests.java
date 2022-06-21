777d633fd5a551982a21e61be7d5d0555b84cbb9
Alejandro Aguirre <aguirreee21@gmail.com>
2022-06-21 20:45:15 -0300

    Create givenListOfContacts_whenGetAllContacts_thenReturnContactsList method

--- src/test/java/com/alkemy/ong/ContactControllerTests.java
+++ src/test/java/com/alkemy/ong/ContactControllerTests.java
@@ -6,6 +6,7 @@ import com.alkemy.ong.controller.ContactController;
 import com.alkemy.ong.dto.ContactDTO;
 import com.alkemy.ong.service.IContactService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +19,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -54,11 +59,7 @@ class ContactControllerTests {
     @Test
     @WithMockUser(setupBefore = TestExecutionEvent.TEST_METHOD, username = "Alejandro")
     void testCreateContact() throws Exception {
-        ContactDTO contactDTO = new ContactDTO();
-        contactDTO.setName("Alejandro");
-        contactDTO.setPhone("+548959526");
-        contactDTO.setEmail("ale@gmail.com");
-        contactDTO.setMessage("Ale, mi buen amigo");
+        ContactDTO contactDTO = createDtoEntity();
 
         given(contactService.save(any(ContactDTO.class)))
                 .willAnswer((invocation)-> invocation.getArgument(0));
@@ -86,11 +87,7 @@ class ContactControllerTests {
     @Test
     @WithMockUser(setupBefore = TestExecutionEvent.TEST_METHOD, username = "Alejandro")
     void testCreateContact2() throws Exception {
-        ContactDTO contactDTO = new ContactDTO();
-        contactDTO.setName("Alejandro");
-        contactDTO.setPhone("+548959526");
-        contactDTO.setEmail("ale@gmail.com");
-        contactDTO.setMessage("Ale, mi buen amigo");
+        ContactDTO contactDTO = createDtoEntity();
 
         when(contactService.save(any())).then(invocation -> {
             ContactDTO c  = invocation.getArgument(0);
@@ -110,4 +107,38 @@ class ContactControllerTests {
 
         verify(contactService).save(any());
     }
+
+    // JUnit test for Get All employees REST API
+    @Test
+    @WithMockUser(setupBefore = TestExecutionEvent.TEST_METHOD, username = "Alejandro")
+    void givenListOfContacts_whenGetAllContacts_thenReturnContactsList() throws Exception{
+        // given - precondition or setup
+        ContactDTO contact1 = createDtoEntity();
+        ContactDTO contact2 = createDtoEntity();
+        List<ContactDTO> listOfContacts = new ArrayList<>();
+        listOfContacts.add(contact1);
+        listOfContacts.add(contact2);
+        given(contactService.getAll()).willReturn(listOfContacts);
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(get("/contacts"));
+
+        // then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()",
+                        CoreMatchers.is(listOfContacts.size())));
+
+    }
+
+    private ContactDTO createDtoEntity() {
+        ContactDTO contactDTO = new ContactDTO();
+        contactDTO.setName("Alejandro");
+        contactDTO.setPhone("+548959526");
+        contactDTO.setEmail("ale@gmail.com");
+        contactDTO.setMessage("Ale, mi buen amigo");
+        return contactDTO;
+    }
+
+
 }
