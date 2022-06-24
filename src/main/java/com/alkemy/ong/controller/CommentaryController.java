857bbc3dd5be9bf7b5dee91d9dc2deadb948e6a1
Diego A. Sejas <dsejas2015@gmail.com>
2022-06-24 00:02:48 -0300

    Solucion de conflictos CommentaryController

--- src/main/java/com/alkemy/ong/controller/CommentaryController.java
+++ src/main/java/com/alkemy/ong/controller/CommentaryController.java
@@ -3,13 +3,13 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.domain.service.ICommentaryService;
 import com.alkemy.ong.domain.util.Url;
 import com.alkemy.ong.dto.CommentaryBodyDTO;
+import com.alkemy.ong.dto.CommentaryDTO;
 import com.alkemy.ong.exception.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import com.alkemy.ong.service.impl.CommentaryServiceImpl;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
@@ -50,7 +50,6 @@ public class CommentaryController {
         return ResponseEntity.ok(commentaryService.save(dto));
     }
 
-}
 
     @GetMapping("/news/{id}")
     public ResponseEntity<?> getCommentaryByPost(@PathVariable Long id) {
